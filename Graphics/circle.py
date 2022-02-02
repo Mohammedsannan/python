3388ff9628ae1da4611cbafab2b73fd824263b4a
Mohammedsannan <97687050+Mohammedsannan@users.noreply.github.com>
2022-02-02 14:47:02 +0530

    Add files via upload

---
+++ Graphics/circle.py
@@ -0,0 +1,9 @@
+def circlearea(r):
+    import math
+    area=math.pi*r*r
+    return area
+
+def circleperi(r):
+    import math
+    perimeter=math.pi*2*r
+    return perimeter
---
+++ Graphics/rectangle.py
@@ -0,0 +1,7 @@
+def rectarea(l,b):
+    area=l*b
+    return area
+
+def rectperi(l,b):
+    perimeter=2*(l+b)
+    return perimeter
